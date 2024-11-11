3406ac759f1a79a93b90664b54e91066b5a735cc
Antoinette Belebele <anthobelebele@gmail.com>
2024-11-11 12:21:33 +0100

    add style in my Home component in 2nd section

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,7 +4,7 @@ function Button() {
   return (
     <div>
         <div>
-            <button type="button" className="text-black bg-greenColor font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 flex gap-4 items-center">Contact Moi         
+            <button type="button" className="text-black bg-greenColor font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 flex gap-4 items-center">Contactez Moi         
                 <FaCircle className="bg-black rounded-full"/>
             </button>
         </div>
--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -4,18 +4,19 @@ import { Link, Outlet } from "react-router-dom"
 
 function Home() {
   return (
-    <div>
-    <div className="p-4 flex gap-10 w-[60rem] ">
-        <div>
-            <p><span className="font-sans font-bold text-3xl">Bonjour, Je suis</span>
-                <br />
-                <span className="font-sans font-bold text-3xl">Antoinette Belebele.</span> <br />
-                <span className="font-paragraph font-extralight text-xs leading-loose w-px">
+    <div className="w-full flex justify-center items-center flex-col">
+    <div className="p-4 flex gap-10 items-center">
+        <div className="w-[34rem]">
+            <div>
+                <p className="font-sans font-bold text-3xl">Bonjour, Je suis</p>
+                <p className="font-sans font-bold text-3xl pt-2">Antoinette Belebele.</p>
+                <p className="font-paragraph font-extralight text-xs leading-loose pt-2">
                 Développeuse web fullstack et web designer passionnée par la création de solutions 
                 innovantes et adaptées aux besoins des utilisateurs. Avec un œil attentif 
                 aux détails et un souci constant de la performance, je conçois des applications 
-                qui allient esthétisme et fonctionnalité pour offrir une expérience utilisateur optimale.</span></p>
-            <div className="flex gap-4">
+                qui allient esthétisme et fonctionnalité pour offrir une expérience utilisateur optimale.</p>
+            </div>
+            <div className="flex gap-4 pt-6">
                 <Button /> 
                 <div className="w-10 h-10 rounded-full bg-grayColor flex items-center justify-center">
                     <FaLinkedinIn className="text-greenColor"/>
@@ -25,9 +26,9 @@ function Home() {
                 </div>
             </div>
         </div>
-        <img src="https://img.freepik.com/photos-gratuite/portrait-femme-affaires-confiant-debout-ses-bras-croises_1098-20791.jpg?ga=GA1.1.46618866.1723015588&semt=ais_hybrid" alt="" className="w-[70rem] h-[38rem]"/>
+        <img src="https://img.freepik.com/photos-gratuite/portrait-femme-affaires-confiant-debout-ses-bras-croises_1098-20791.jpg?ga=GA1.1.46618866.1723015588&semt=ais_hybrid" alt="" className="w-[37rem] h-[38rem] rounded-lg"/>
     </div>
-    <hr className="border-grayColor"/> 
+    <hr className="border-grayColor w-full"/> 
     <div className="p-4">
         <h1 className="font-sans font-bold text-3xl">Projets Réalisés</h1>
         <div className="flex gap-3 items-center">
@@ -43,7 +44,7 @@ function Home() {
             <Outlet />
         </div>
     </div>
-    <hr className="border-grayColor"/> 
+    <hr className="border-grayColor w-full"/> 
     </div>
   )
 }
